98982b440d1218601345f0c5ed7cb76fffc09b1c
marcenappia <marcenapp.ia@gmail.com>
2026-09-06 09:58:14 -0300

    feat: register public marcenaria profile route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
+import MarceneiroPublico from "./pages/MarceneiroPublico";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,6 +20,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/auth" element={<Auth />} />
+            <Route path="/marceneiro/:slug" element={<MarceneiroPublico />} />
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
